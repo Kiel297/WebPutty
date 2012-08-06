92580bce3e68730cdba7710fda4885e718697e27
Kiel297 <kielchurch_1@hotmail.co.uk>
2012-08-06 02:49:59 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,14 +7,14 @@ debug_profiler_enabled = False
 appname = 'WebPutty OSS'
 appversion_raw = [1, 3, 0]
 appversion = '.'.join([str(num) for num in appversion_raw])
-invite_sender_email = '%s Invitation <you@example.com>' % appname
-incoming_sender_email = '%s Incoming Mail <you@example.com>' % appname
+invite_sender_email = '%s Invitation <kielchurch_1@hotmail.co.uk>' % webputtykiel
+incoming_sender_email = '%s Incoming Mail <kielchurch_1@hotmail.co.uk>' % webputtykiel
 log_all_incoming = True
 # List of admins to forward mail to.
-forward_mail_to = ['you@example.com']
+forward_mail_to = ['kielchurch_1@hotmail.co.uk']
 jquery_url = '//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'
 # Generate this once by calling os.urandom(24)
-secret_key = "Shhh... It's a secret."
+secret_key = "wordlife1"
 
 # Name of the Google Cloud Storage bucket
 use_google_cloud_storage = False
